c5f87c5d8c05c81ef768eff6294f4df325904046
Armandas <armandas.rokas@gmail.com>
2019-10-02 11:22:19 +0200

    Refactored to int to Timestamp

---
+++ app/src/main/java/dk/dtu/group22/beeware/data/entities/User.java
@@ -0,0 +1,8 @@
+package dk.dtu.group22.beeware.data.entities;
+
+public class User {
+
+    private int id;
+    private String username;
+
+}
--- app/src/main/java/dk/dtu/group22/beeware/data/repositories/repoImpl/HiveRepoSqlLiteImpl.java
+++ app/src/main/java/dk/dtu/group22/beeware/data/repositories/repoImpl/HiveRepoSqlLiteImpl.java
@@ -1,13 +1,22 @@
 package dk.dtu.group22.beeware.data.repositories.repoImpl;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import dk.dtu.group22.beeware.data.entities.Hive;
 import dk.dtu.group22.beeware.data.repositories.interfaceRepo.HiveRepository;
 
 public class HiveRepoSqlLiteImpl implements HiveRepository {
+
     @Override
-    public List<Hive> getHives() {
+    public List<Hive> getHives(int userId) {
         return null;
     }
+
+    @Override
+    public Hive getHive(int hiveId, Timestamp sinceTime, Timestamp untilTime) {
+        return null;
+    }
+
+
 }
