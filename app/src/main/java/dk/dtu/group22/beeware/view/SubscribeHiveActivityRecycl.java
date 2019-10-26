a67a5c9af0aea1f99f73cd017227f2ea04614bbb
Armandas <armandas.rokas@gmail.com>
2019-10-26 18:26:51 +0200

    added recycl view

---
+++ app/src/main/java/dk/dtu/group22/beeware/view/SubscribeHiveActivityRecycl.java
@@ -0,0 +1,16 @@
+package dk.dtu.group22.beeware.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dk.dtu.group22.beeware.R;
+
+public class SubscribeHiveActivityRecycl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscribe_hive_recycl);
+    }
+}
