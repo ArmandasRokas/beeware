4e1638dc65c0067867532403f2e126bf75989d6e
Christoffer Rasmussen <christoffer@irvall.com>
2019-10-26 21:50:39 +0200

    Cleaned up CSV related methods and added documentation

--- app/src/main/java/dk/dtu/group22/beeware/data/repositories/repoImpl/HiveRepoCSVImpl.java
+++ app/src/main/java/dk/dtu/group22/beeware/data/repositories/repoImpl/HiveRepoCSVImpl.java
@@ -34,60 +34,66 @@ public class HiveRepoCSVImpl implements HiveRepository {
         return idToHive.get(id);
     }
 
+    /**
+     * @param file
+     * @return a Hive instance, which is the unpacked value of the given CSV-filename
+     */
     Hive extractHiveFromHiveToolCSV(String file) {
-        Scanner scanner;
+        Scanner scanner = null;
         try {
             scanner = new Scanner(new File(file));
-
-            final int timestampIndex = 0;
-            final int weightIndex = 1;
-            final int tempIndex = 8;
-            final int humidityIndex = 9;
-            //TODO: Find index for illuminance
-            final int illuminanceIndex = 6;
-
-            List<Measurement> data_measure = new ArrayList<>();
-            //Remove the initial line of categories
-            scanner.nextLine();
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                line.replaceAll("\\s", "");
-                String[] raw_data = line.split(",");
-
-                //Example Hive tool entry: 2019-09-25 00:00:01
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
-                Timestamp timestamp = new Timestamp(0);
-                try {
-                    Date date = dateFormat.parse(raw_data[timestampIndex]);
-                    timestamp = new Timestamp(date.getTime());
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
-                Double weightLbs = parseToDoubleOrNeg(raw_data[weightIndex]);
-                Double weightKg = parseToDoubleOrNeg(raw_data[weightIndex]) * 0.45359237;
-
-                Double tempF = parseToDoubleOrNeg(raw_data[tempIndex]);
-                Double tempC = (tempF - 32.0) * 5 / 9;
-
-                Double humidity = parseToDoubleOrNeg(raw_data[humidityIndex]);
-
-                Double illuminance = parseToDoubleOrNeg(raw_data[illuminanceIndex]);
-
-                data_measure.add(new Measurement(timestamp, weightKg, tempC, humidity, illuminance));
-            }
-            for (Measurement measure : data_measure) {
-                System.out.println(measure);
-            }
-            Hive hive = new Hive();
-            hive.setMeasurements(data_measure);
-            return hive;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return null;
+
+        final int timestampIndex = 0;
+        final int weightIndex = 1;
+        final int tempIndex = 8;
+        final int humidityIndex = 9;
+        final int illuminanceIndex = 6;
+
+        List<Measurement> data_measure = new ArrayList<>();
+
+        //Remove the initial line of categories
+        scanner.nextLine();
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            line.replaceAll("\\s", "");
+            String[] raw_data = line.split(",");
+
+            //Example Hive tool entry: 2019-09-25 00:00:01
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+            Timestamp timestamp = new Timestamp(0);
+            try {
+                Date date = dateFormat.parse(raw_data[timestampIndex]);
+                timestamp = new Timestamp(date.getTime());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            Double weightLbs = parseToDoubleOrNeg(raw_data[weightIndex]);
+            Double weightKg = parseToDoubleOrNeg(raw_data[weightIndex]) * 0.45359237;
+
+            Double tempF = parseToDoubleOrNeg(raw_data[tempIndex]);
+            Double tempC = (tempF - 32.0) * 5 / 9;
+
+            Double humidity = parseToDoubleOrNeg(raw_data[humidityIndex]);
+
+            Double illuminance = parseToDoubleOrNeg(raw_data[illuminanceIndex]);
+
+            data_measure.add(new Measurement(timestamp, weightKg, tempC, humidity, illuminance));
+        }
+        Hive hive = new Hive();
+        hive.setMeasurements(data_measure);
+        return hive;
     }
 
+    /**
+     * @param string
+     * @return either a double, or a symbolic value (-1),
+     * which indicate that the value trying to be parsed was incorrectly formatted or non-existing.
+     */
     Double parseToDoubleOrNeg(String string) {
         try {
             return Double.parseDouble(string);
