a3b7daae06a4ea9765df00120e1fd1b7bf301e2d
DAFA <s185120@student.dtu.dk>
2019-10-29 18:50:20 +0100

    Rettet rund knap og givet elevation. Lavet afrundet knap

---
+++ app/src/main/java/dk/dtu/group22/beeware/PersonalSettings.java
@@ -0,0 +1,14 @@
+package dk.dtu.group22.beeware;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_settings);
+    }
+}
