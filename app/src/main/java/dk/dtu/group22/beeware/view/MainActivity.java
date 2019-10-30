73aa0cf4940d27cd2f6ad9628b48370532e2c63a
JustusGammelgaard <justusgammelgaard@hotmail.com>
2019-10-30 09:31:27 +0100

    Merge branch 'master' of C:\Users\justu\AndroidStudioProjects\beeware2 with conflicts.

--- app/src/main/java/dk/dtu/group22/beeware/view/MainActivity.java
+++ app/src/main/java/dk/dtu/group22/beeware/view/MainActivity.java
@@ -1,23 +1,37 @@
 package dk.dtu.group22.beeware.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 import dk.dtu.group22.beeware.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    GridView gridView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
+        setContentView(R.layout.activity_hive_overview);
+
+        gridView = findViewById(R.id.gridView);
+
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+        gridView.setAdapter(imageAdapter);
+
     }
 }
+
