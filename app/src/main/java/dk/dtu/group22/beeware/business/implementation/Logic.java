832355429287c64f844b61b9b3e71376df556d28
Christoffer Rasmussen <christoffer@irvall.com>
2019-11-11 10:55:35 +0100

    Reversed some logic

--- app/src/main/java/dk/dtu/group22/beeware/business/implementation/Logic.java
+++ app/src/main/java/dk/dtu/group22/beeware/business/implementation/Logic.java
@@ -78,7 +78,7 @@ public class Logic implements ILogic {
         Timestamp prevTime = hive.getMeasurements().get(prevMidnightIndex).getTimestamp();
         Timestamp prevprevTime = hive.getMeasurements().get(prevprevMidnightIndex).getTimestamp();
 
-        if(isAroundMidnight(prevTime, today) && isAroundMidnight(prevprevTime, yesterday)){
+        if(!(isAroundMidnight(prevTime, today) && isAroundMidnight(prevprevTime, yesterday))){
            hive.setWeightDelta(Double.NaN);
         }else {
             double prevMidnightWeight = hive.getMeasurements().get(prevMidnightIndex).getWeight();
