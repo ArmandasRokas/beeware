ca921f185d2bc08e4d05fd696d4e4ba789b43bac
Armandas <armandas.rokas@gmail.com>
2019-11-13 19:17:23 +0100

    added curr temp and lux values on main screen

--- app/src/main/java/dk/dtu/group22/beeware/presentation/ImageAdapter.java
+++ app/src/main/java/dk/dtu/group22/beeware/presentation/ImageAdapter.java
@@ -56,6 +56,8 @@ public class ImageAdapter extends BaseAdapter {
         ImageView img = gridView.findViewById(R.id.album);
         TextView title = gridView.findViewById(R.id.album_title);
         TextView weight = gridView.findViewById(R.id.hive_currWeightTV);
+        TextView temp = gridView.findViewById(R.id.hive_currTempTV);
+        TextView illum = gridView.findViewById(R.id.hive_currIllumTV);
 
         img.setImageResource(R.drawable.beehive2);
         img.setOnClickListener(new View.OnClickListener() {
@@ -81,6 +83,8 @@ public class ImageAdapter extends BaseAdapter {
         }
 
         weight.setText(formattedWeightDisplay);
+        temp.setText(String.format("%.1f\u2103(inside)",hives.get(position).getCurrTemp()));
+        illum.setText(String.format("%.0flx", hives.get(position).getCurrIlluminance()));
 
         return gridView;
     }
