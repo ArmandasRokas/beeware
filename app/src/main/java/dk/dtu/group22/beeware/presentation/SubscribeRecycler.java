b03180caa5f16578aee3de1afdd5b2905809473f
DAFA <s185120@student.dtu.dk>
2019-11-16 18:13:47 +0100

    Struktureret subscribe kode og lavet egen back arrow samt forberedt til at lave søge funktion -David

--- app/src/main/java/dk/dtu/group22/beeware/presentation/SubscribeRecycler.java
+++ app/src/main/java/dk/dtu/group22/beeware/presentation/SubscribeRecycler.java
@@ -2,18 +2,15 @@ package dk.dtu.group22.beeware.presentation;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -24,7 +21,7 @@ import dk.dtu.group22.beeware.R;
 import dk.dtu.group22.beeware.business.implementation.Logic;
 import dk.dtu.group22.beeware.dal.dto.interfaces.NameIdPair;
 
-public class SubscribeRecycler extends AppCompatActivity {
+public class SubscribeRecycler extends AppCompatActivity implements View.OnClickListener {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -32,34 +29,35 @@ public class SubscribeRecycler extends AppCompatActivity {
     private Logic logic;
     private TextView errorTv;
     private ProgressBar progressBar;
+    private ImageView backArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        logic = Logic.getSingleton();
-        logic.setContext(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscribe);
-        setupToolbar();
+
+        // Initialisation
+        logic = Logic.getSingleton();
+        logic.setContext(this);
 
         errorTv = findViewById(R.id.errorSubscribeHives);
         progressBar = findViewById(R.id.indeterminateBar);
         recyclerView = findViewById(R.id.hivesToSubRV);
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
+
         recyclerView.setHasFixedSize(true);
-        // use a linear layout manager
+        // Linear layout manager for the recycler view
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        /*
-        List<HighScore> highScores = highScoreRepo.getHighScores();
-        String[] highScoresStringArray = highScores.stream()
-                .map(x -> String.format("%s %s", x.getUsername(), x.getScore()))
-                .toArray(String[]::new);
-        */
+        addListElements();
 
+        backArrow = findViewById(R.id.subscribe_back_arrow);
+        backArrow.setOnClickListener(this);
+    }
 
+    // Gets the names and ids that is possible to subscribe to,
+    // and adds them to the list via the 'SubscribeAdapter' class
+    private void addListElements() {
         new AsyncTask() {
             List<NameIdPair> hivesToSub;
             String errorMsg = null;
@@ -92,95 +90,62 @@ public class SubscribeRecycler extends AppCompatActivity {
                 }
             }
         }.execute();
-
     }
 
-    // Replaces action bar with custom_toolbar and sets the title of the activity right
-    public void setupToolbar() {
-        // Sets the custom_toolbar for the activity
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        // Calculate ActionBar's height
-        TextView toolbar_title = findViewById(R.id.toolbar_title);
-        Toolbar.LayoutParams params = new Toolbar.LayoutParams(Toolbar.LayoutParams.MATCH_PARENT, Toolbar.LayoutParams.MATCH_PARENT);
-        int actionBarHeight = 0;
-        TypedValue tv = new TypedValue();
-        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
-            actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
-        }
-        params.setMarginEnd(actionBarHeight + 25);
-        toolbar_title.setLayoutParams(params);
-
-        // account logo button left side on custom_toolbar
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back_arrow);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        toolbar_title.setText("Subscriptions");
-    }
-
-    // When back arrow button is pressed
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                break;
+    public void onClick(View view) {
+        if (view == backArrow) {
+            finish();
         }
-        return super.onOptionsItemSelected(item);
     }
-
 }
 
 class SubscribeAdapter extends RecyclerView.Adapter<SubscribeAdapter.MyViewHolder> {
+
     private ArrayList<Integer> subbedIds;
     private List<NameIdPair> mDataset;
-    private Logic logic = Logic.getSingleton();
+    private Logic logic;
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public SubscribeAdapter(List<NameIdPair> myDataset) {
+        mDataset = myDataset;
+        logic = Logic.getSingleton();
+        subbedIds = logic.getSubscriptionIDs();
+    }
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class MyViewHolder extends RecyclerView.ViewHolder {
+
         // each data item is just a string in this case
-        //public TextView textView;
         public TextView subHiveName;
         public Switch subHiveSwitch;
 
         public MyViewHolder(View v) {
             super(v);
-            //textView = v.findViewById(R.id.hiveNameTv);
             subHiveName = v.findViewById(R.id.subscribe_name);
-            // TODO: Når recycleren genbruger switch'ene, tickes de ikke tilbage på off
             subHiveSwitch = v.findViewById(R.id.subscribe_switch);
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public SubscribeAdapter(List<NameIdPair> myDataset) {
-        mDataset = myDataset;
-        subbedIds = logic.getSubscriptionIDs();
-    }
-
     // Create new views (invoked by the layout manager)
     @Override
-    public SubscribeAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
-                                                            int viewType) {
+    public SubscribeAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.custom_subscribe, parent, false);
-        MyViewHolder vh = new MyViewHolder(v);
-        return vh;
+
+        return new MyViewHolder(v);
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
+
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        //System.out.println((mDataset.get(position).getName()));
-        //holder.textView.setText(mDataset.get(position).getName());
         holder.subHiveName.setText(mDataset.get(position).getName());
 
         // Checks if the current list position element is in list of subbed ids
@@ -193,6 +158,7 @@ class SubscribeAdapter extends RecyclerView.Adapter<SubscribeAdapter.MyViewHolde
             }
         }
 
+        // Runs when the subscribe switch is being switched on/off
         holder.subHiveSwitch.setOnClickListener(
                 v -> {
                     System.out.println("The switch is set to " + holder.subHiveSwitch.isChecked());
@@ -206,8 +172,6 @@ class SubscribeAdapter extends RecyclerView.Adapter<SubscribeAdapter.MyViewHolde
                     subbedIds = logic.getSubscriptionIDs();
                 }
         );
-
-
     }
 
     // Return the size of your dataset (invoked by the layout manager)
