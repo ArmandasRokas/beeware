a905fc972fa90ca17d334f142521ca5bd50ad4ca
Andreas <Gerneeer@gmail.com>
2019-11-27 11:02:54 +0100

    Written tests to all public methods in Logic.

--- app/src/test/java/dk/dtu/group22/beeware/business/implementation/LogicTest.java
+++ app/src/test/java/dk/dtu/group22/beeware/business/implementation/LogicTest.java
@@ -2,13 +2,21 @@ package dk.dtu.group22.beeware.business.implementation;
 
 import org.junit.Test;
 
+import java.sql.Array;
+import java.sql.Time;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.jar.Attributes;
 
 import dk.dtu.group22.beeware.dal.dao.Hive;
 import dk.dtu.group22.beeware.dal.dao.Measurement;
 import dk.dtu.group22.beeware.dal.dto.implementation.HiveCached;
+import dk.dtu.group22.beeware.dal.dto.interfaces.NameIdPair;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -158,6 +166,75 @@ public class LogicTest {
         // Clean up
         HiveCached hiveCached = HiveCached.getSingleton();
         hiveCached.cleanCachedHives();
+    }
+
+    public void testGetHive(){
+
+        // Testing, if "getHive()" will use its methods correctly, and return a full hive with
+        // initialized values.
+        Logic logic = Logic.getSingleton();
+
+        long now = System.currentTimeMillis();
+
+        Hive hive = logic.getHive(240, new Timestamp(now - 300000), new Timestamp(now));
+
+        assertTrue(hive.getName().equals("FHA_Stade1"));
+        assertFalse(hive.getMeasurements() == null);
+
+    }
+
+    public void testSubscribeHive(){
+        Logic logic = Logic.getSingleton();
+
+        // Test, that if i subscribe one hive with id = 240. then i only get that one subscribtion.
+        logic.subscribeHive(240);
+        assertEquals(logic.getSubscriptionIDs() , Arrays.asList(240));
+
+        // Test that there are only 1 subscribed hive, and that it is the correct one (id 240 is FHA_Stade1)
+        // and, that it has measurements.
+        List<Hive> hives = logic.getSubscribedHives(1);
+        assertTrue(hives.size() == 1);
+        assertTrue(hives.get(0).getName() == "FHA_Stade1");
+        assertFalse(hives.get(0).getMeasurements() == null);
+
+        // Test, that if i now unsubscribe the hive, the list of subscribed hives and subscribed ids are now empty.
+        logic.unsubscribeHive(240);
+        List<Integer> emptyIDs = new ArrayList<>();
+        List<Hive> emptyHives = new ArrayList<>();
+        assertEquals(logic.getSubscriptionIDs(), emptyIDs);
+        assertEquals(logic.getSubscribedHives(1), emptyHives);
+
+    }
+
+    public void testgetNamesAndIDs(){
+        Logic logic = Logic.getSingleton();
+
+        // getNamesAndIDs() is called, in order to recieve a list of names and ids from Hivetool.net
+        // Firstly, we test whether the list contains several active hives.
+        List<NameIdPair> namesAndIDs = logic.getNamesAndIDs();
+        NameIdPair FHAStade1 = new NameIdPair("FHA_Stade1", 240);
+        NameIdPair Athens = new NameIdPair("Athens", 10);
+        NameIdPair Colony1 = new NameIdPair("Colony1", 176);
+        NameIdPair PetesBees = new NameIdPair("PetesBees", 112);
+
+        assertTrue(namesAndIDs.contains(FHAStade1));
+        assertTrue(namesAndIDs.contains(Athens));
+        assertTrue(namesAndIDs.contains(Colony1));
+        assertTrue(namesAndIDs.contains(PetesBees));
+
+        // Secondly, we test to see if the list also contains inactive hives.
+
+        NameIdPair Deseret = new NameIdPair("Deseret", 147);
+        NameIdPair England = new NameIdPair("England", 128);
+        NameIdPair Judge = new NameIdPair("Judge", 101);
+        NameIdPair Radiance = new NameIdPair("Radiance", 38);
+
+        assertTrue(namesAndIDs.contains(Deseret));
+        assertTrue(namesAndIDs.contains(England));
+        assertTrue(namesAndIDs.contains(Judge));
+        assertTrue(namesAndIDs.contains(Radiance));
+
+
     }
 
 
